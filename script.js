3185f0130ea3a14a3cef5cb662356aca9a4ec640
ARYAN PATEL <patelaryan7751@gmail.com>
2023-02-15 17:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,8 @@ const displayEntries = () => {
   <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Name</th>
             <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Email</th>
             <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Password</th>
-            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">dob</th>
-            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">accepted terms ?</th>
+            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Dob</th>
+            <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">Accepted terms ?</th>
           </tr>
           </thead>
           <tbody>
